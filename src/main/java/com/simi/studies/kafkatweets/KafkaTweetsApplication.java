457d4da4d4ff2da4f684c887cc5894019f0f82e3
Felipe Miranda <felip.simi@gmail.com>
2020-10-09 21:13:52 -0300

    Preparacao: setup do projeto no Spring Initializr

---
+++ src/main/java/com/simi/studies/kafkatweets/KafkaTweetsApplication.java
@@ -0,0 +1,13 @@
+package com.simi.studies.kafkatweets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTweetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaTweetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simi/studies/kafkatweets/KafkaTweetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simi.studies.kafkatweets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTweetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
